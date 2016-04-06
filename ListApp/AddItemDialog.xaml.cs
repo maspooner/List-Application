9cf7c7aa6f1ea92137115b88e1a7d25211a1355b
Matt <matt.spooner10@gmail.com>
2016-04-06 09:30:32 -0400

    More work on that dialog

--- ListApp/AddItemDialog.xaml.cs
+++ ListApp/AddItemDialog.xaml.cs
@@ -26,19 +26,73 @@ namespace ListApp {
 			if(Owner == null) {
 				Owner = mainWindow;
 			}
+			CreateElements(template);
 			ShowDialog();
 			if (DialogResult.Value) {
+				Console.WriteLine("OK");
 				//TODO implement
+				foreach(ItemTemplateItem iti in template) {
+					object field = FindName(iti.Name + "_ui");
+				}
 				return null;
 			}
 			else {
 				return null;
 			}
 		}
+		private void CreateElements(List<ItemTemplateItem> template, ListItem li = null) {
+			contentPanel.Children.Clear();
+			for (int i = 0; i < template.Count; i++) {
+				ItemTemplateItem iti = template[i];
+				UIElement uie = null;
+				switch (iti.Type) {
+					case ItemType.BASIC:
+					case ItemType.DATE:
+						uie = new TextBlock();
+						if(li != null) {
+							(uie as TextBlock).Text = li[i].GetValue().ToString();
+						}
+						break;
+					case ItemType.ENUM:
+						ComboBox cb = new ComboBox();
+						cb.ItemsSource = iti.Metadata as string[];
+						cb.SelectedIndex = 0;
+						uie = cb;
+						break;
+					case ItemType.IMAGE:
+
+						break;
+				}
+				uie.SetValue(NameProperty, iti.Name + "_ui");
+				switch (iti.Type) {
+					case ItemType.BASIC:
+					case ItemType.DATE:
+					case ItemType.ENUM:
+						StackPanel sp = new StackPanel();
+						sp.Orientation = Orientation.Horizontal;
+						Label l = new Label();
+						l.Content = iti.Name + ": ";
+						sp.Children.Add(l);
+						sp.Children.Add(uie);
+						contentPanel.Children.Add(sp);
+						break;
+					case ItemType.IMAGE:
+
+
+						break;
+				}
+			}
+		}
+		private bool IsValidInput() {
+			//TODO perform validation
+			foreach(UIElement uie in contentPanel.Children) {
+
+			}
+		}
 		//WPF
 		private void ConfirmButton_Click(object sender, RoutedEventArgs e) {
-			//TODO perform validation
-			DialogResult = true;
+			
+			DialogResult = IsValidInput();
 		}
 	}
 }
